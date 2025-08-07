a2d3adc1160d46da5303c549a3e1ced7d9cd0020
FranManzanares <fmanzanares@pioix.edu.ar>
2025-08-07 10:22:58 -0300

    hola

---
+++ src/app/componentes/Button.js
@@ -0,0 +1,14 @@
+export default function Button(promp) {
+    let promp = {
+        text: "BOTON"
+    }
+    
+    
+    return (
+        <>
+            <button>HOLA</button>
+        </>
+    )
+
+    
+}
---
+++ src/app/login/page.js
@@ -0,0 +1,19 @@
+import Button from "../componentes/Button";
+
+export default function login() {
+    return (
+        <>
+            <h1>Este es el primer boton que hago</h1>
+            <Button></Button>
+
+
+
+
+        </>
+
+
+
+
+    )
+
+}
